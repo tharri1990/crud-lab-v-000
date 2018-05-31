5d51035ea0fdd8f57fbf87c6de5235a345e25f6f
Tavish Harrison <creationmargin26@gmail.com>
2018-05-31 00:50:09 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+    const associatedReviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    const reviews = associatedReviews.map((review, index) => {
+      return <Review key={index} review={review} store={this.props.store} />
+    })
     return (
       <ul>
-        Reviews
+        {reviews}
       </ul>
-    );
+    )
   }
-};
+}
 
-export default Reviews;
+export default Reviews
